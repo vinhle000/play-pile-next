5193031008dd69bb2939bdc412de550d87414f7c
vinhle000 <vinhle000@gmail.com>
2024-04-16 10:10:50 -0700

    -Updated userGameModel to have playing and played statuses -Added initial userGameDataEdit modal

--- backend/src/models/userGameModel.js
+++ backend/src/models/userGameModel.js
@@ -12,11 +12,36 @@ const UserGameSchema = new mongoose.Schema(
       type: String,
       enum: ['Playing', 'Completed', 'Dropped', 'Plan to Play'],
       default: 'Plan to Play',
-      // required: true
     },
     isInBacklog: {
       type: Boolean,
       default: false,
+    },
+    playingStatus: {
+      type: String,
+      enum: ['Not started', 'Currently Playing', 'Paused', 'Endless', 'Replaying'],
+      default: 'Not started',
+    },
+    playedStatus: {
+      type: String,
+      enum: ['No status', 'Finished', 'Completed', 'Dropped'],
+      default: 'No status',
+    },
+    playDates: {
+      type: [
+        {
+          startDate: {
+            type: Date,
+          },
+          endDate: {
+            type: Date,
+          },
+        },
+      ],
+    },
+    hoursPlayed: {
+      type: Number,
+      default: 0,
     },
     notes: {
       type: String,
--- frontend/src/components/UserGameDataEditModal.jsx
+++ frontend/src/components/UserGameDataEditModal.jsx
@@ -1,38 +1,117 @@
 import React, {useState} from 'react'
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+import logRocket from 'logrocket'
+
+
+// function Modal({children, onClose}) {
+
+//   return (
+//     <div className="fixed inset-0 bg-black bg-opacity-20 z-50 flex justify-center items-center">
+//         <div className="bg-white p-5 rounded">
+//           {children}
+//           <div> </div>
+//         <div>
+//             <label> Playing Status:  </label>
+//             <select
+//               name="playingStatus"
+//               value={userGameData.playingStatus} onChange={(e) => setUserGameData({...userGameData, playingStatus: e.target.value})}
+//             >
+
+//             </select>
+//         </div>
+//         <div> {}</div>
+
+//           <button
+//             className="mt-5 px-4 py-2 bg-red-400 text-white rounded hover:bg-red-700"
+//             onClick={onClose}
+//           >
+//             Close
+//           </button>
+//         </div>
+//     </div>
+//   )
+// }
+
+
+function UserGameDataEditModal({userGameData, setUserGameData, onClose}) {
+  const [formData, setFormData] = useState({
+    playingStatus: userGameData.playingStatus,
+    playedStatus: userGameData.playedStatus,
+    dates: userGameData.dates,
+    notes: userGameData.notes,
+  })
 
 
 
-function Modal({children, onClose}) {
+  //TODO: On save, update the userGameData
+  const updateUserGameData = async (igdbId, updateData) => {
+    updateData ? updateData : {}
+    try {
+      //NOTE: Only rerenders the GameCard component in the list that has the updated changes
+      // Utilizing useUserGameData hook to manage the state of the game data
+      // with the use of React.useMemo and React.useCallback
+     let newData = await userGameService.updateUserGameData(igdbId, {...updateData})
 
-  return (
-    <div className="fixed inset-0 bg-black bg-opacity-20 z-50 flex justify-center items-center">
-        <div className="bg-white p-5 rounded">
-          {children}
-          <button
-            className="mt-5 px-4 py-2 bg-red-400 text-white rounded hover:bg-red-700"
-            onClick={onClose}
-          >
-            Close
-          </button>
-        </div>
-    </div>
-  )
-}
+     setUserGameData({...userGameData, ...newData})
+     console.log('GameCard -> updateBacklog -> newData', newData)
+      LogRocket.log('userGameData updated successfully', newData);
 
+    } catch (error) {
+      console.error('Error updating UserGame Data ', error)
+      LogRocket.error('Error updating UserGame Data ', error);
+    }
+  }
+//     <div className="fixed inset-0 bg-black bg-opacity-20 z-50 flex justify-center items-center">
+//         <div className="bg-white p-5 rounded">
 
-function UserGameDataEditModal({isOpen, onClose, userGameData}) {
   return (
     <>
-      <Modal onClose={onClose}>
-        <div> Title</div>
-        <div> Statuses </div>
-        <div> Dates</div>
-        <div> Notes</div>
-        <div> remove close save</div>
-        <div> {}</div>
+      <Dialog className="fixed inset-0 z-50 overflow-y-auto">
+        <div className="fixed inset-0 flex items-center justify-center bg-black/30" >
 
-      </Modal>
+          <div className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+            <DialogTitle className="text-lg font-medium leading-6 text-gray-900">
+              Edit Log
+            </DialogTitle>
 
+            <div className="mt-4 space-y-4">
+              <div>
+                <Label htmlFor="notes">Notes</Label>
+                <Input
+                  id="notes"
+                  name="notes"
+                  value={formData.notes}
+                  onChange={()=> setFormData({...formData, notes: e.target.value})}
+                />
+              </div>
+            </div>
+
+            <div className="mt-4 flex justify-end gap-2">
+              <Button variant="secondary" onClick={onClose}>
+                Close
+              </Button>
+              <Button onClick={() => updateUserGameData()}>Save</Button>
+            </div>
+
+         </div>
+
+
+
+
+        </div>
+      </Dialog>
     </>
 
   )
