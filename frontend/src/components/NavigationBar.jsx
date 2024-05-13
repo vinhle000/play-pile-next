0808105498c08641579f5c0eddb2648e9b47c737
vinhle000 <vinhle000@gmail.com>
2024-05-13 15:00:49 -0700

    Further navigation styling for backup Avatar user Icon

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -23,25 +23,28 @@ import  UserContext  from '../contexts/UserContext';
   //useContext to get userLoggedIn state
 
 function AvatarDropdownMenu() {
-
   const { user, loading, logout } = useContext(UserContext);
+  console.log(user)
   return (
 
       <DropdownMenu>
         <DropdownMenuTrigger>
+            <div className="h-full w-full">
           <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>
-              <UserCircleIcon className="px-3 py-2 text-gray-400 rounded-lg hover:bg-gray-700" />{' '}
-              {/*//FIXME styling*/}
+
+            <AvatarImage src="https://github.com/shadcn.pn" />
+            <AvatarFallback className="w-full h-full flex justify-center items-center
+               text-white bg-blue-800 rounded-x hover:bg-gray-700" >
+                {user.username[0].toUpperCase()}
             </AvatarFallback>
           </Avatar>
+            </div>
         </DropdownMenuTrigger>
         <DropdownMenuContent>
           <DropdownMenuLabel>My Account</DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuItem>
-            <Link to="/profile">Board</Link>
+            <Link to="/board">Board</Link>
           </DropdownMenuItem>
           <DropdownMenuItem>
             <Link to="/playPileBoard">Play Pile</Link>
@@ -76,7 +79,7 @@ function NavigationBar() {
                 <NavigationMenuItem key={'playPile'}>
                 <Link
                     to={'/playPile'}
-                    className="w-14 h-6 font-extrabold text-black/60 hover:text-white/60"
+                    className="w-14 h-6 font-extrabold text-2xl text-black/60 hover:text-white/60"
                   >
                     {'PlayPile'}
                   </Link>
@@ -86,7 +89,7 @@ function NavigationBar() {
                 <NavigationMenuItem key={'board'}>
                 <Link
                     to={'/board'}
-                    className="w-14 h-6 font-medium text-black/60 hover:text-white/60"
+                    className="w-14 h-6 font-medium text-lg text-black/60 hover:text-white/60"
                   >
                       <div>Board</div>
                   </Link>
@@ -104,10 +107,10 @@ function NavigationBar() {
       <NavigationMenuList className="flex items-center justify-center self-stretch">
         {user
         ? (
-            <>
+            <div className="flex space-x-3">
               <SearchBar />
               <AvatarDropdownMenu/>
-            </>
+            </div>
           )
         : (
           <>
