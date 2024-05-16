eae04f1a0e848a7b460849657912c60039a9a0f4
Vinh Le <vinhle000@gmail.com>
2024-05-16 16:05:50 -0700

    Update userGameService.js

--- frontend/src/services/userGameService.js
+++ frontend/src/services/userGameService.js
@@ -24,6 +24,11 @@ const userGameService = {
     const body = { columnIds: columnIds };
     try {
       const response = await axios.post(`${API_URL}/column/`, body, { withCredentials: true });
+        if (response.data && Array.isArray(response.data.items)) {
+           console.log(response.data.items);
+         } else {
+          console.error('Unexpected response format:', response.data);
+         }
       return response.data;
     } catch (error) {
       console.error('Error getting user game by column ids', error);
@@ -33,6 +38,12 @@ const userGameService = {
   async getUserGamesOnBoard() {
     try {
       const response = await axios.get(`${API_URL}/board`, { withCredentials: true });
+        if (response.data && Array.isArray(response.data.items)) {
+           console.log(response.data.items);
+         } else {
+          console.error('Unexpected response format:', response.data);
+         }
+      
       return response.data;
     } catch (error) {
       console.error('Error getting user games on board', error);
