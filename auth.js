3df6560306854caf1452a5408e0677f002e36972
vinhle000 <vinhle000@gmail.com>
2024-07-12 13:07:31 -0700

    * Fix for assigning callbacks field explicitly for the NextAuth options
    *Added protected route for getting user profile info
    
    *Removed Page routes

--- auth.js
+++ auth.js
@@ -4,39 +4,56 @@ import Credentials from 'next-auth/providers/credentials';
 import { connectDB } from '@/lib/db';
 import User from '@/lib/models/userModel';
 import Column from '@/lib/models/columnModel';
+import mongoose from 'mongoose';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
-  async signIn({ user, account, profile }) {
-    await connectDB();
-    let existingUser = await User.findOne({ email: profile.email });
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      await connectDB();
+      let existingUser = await User.findOne({ email: profile.email });
 
-    if (!existingUser) {
-      const newUser = await User.create({ email: profile.email });
+      if (!existingUser) {
+        const newUser = await User.create({ email: profile.email });
 
-      // Create initial three columns for the user.
-      const defaultColumnTitles = ['Backlog', 'Playing', 'Done'];
+        // Create initial three columns for the user.
+        const defaultColumnTitles = ['Backlog', 'Playing', 'Done'];
 
-      for (const [index, title] of defaultColumnTitles.entries()) {
-        await Column.create({
-          userId: newUser._id,
-          title: title,
-          onBoard: true,
-          position: index,
-        });
+        for (const [index, title] of defaultColumnTitles.entries()) {
+          await Column.create({
+            userId: newUser._id,
+            title: title,
+            onBoard: true,
+            position: index,
+          });
+        }
       }
-    }
-    return true;
-  },
-  async jwt({ token, user }) {
-    if (user) {
-      const existingUser = await User.findOne({ email: user.email });
-      token.id = existingUser._id;
-    }
-    return token;
-  },
-  async session({ session, token }) {
-    session.user.id = token.id;
-    return session;
+      return true;
+    },
+    async jwt({ token, user, account }) {
+      if (user) {
+        await connectDB();
+        const existingUser = await User.findOne({ email: user.email });
+        if (existingUser) {
+          token.id = existingUser._id.toString(); // Convert ObjectId to string
+        }
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token?.id) {
+        try {
+          // Convert string back to ObjectId, otherwise set to null
+          session.user.id = new mongoose.Types.ObjectId(token.id);
+        } catch (error) {
+          console.error('Error converting to ObjectId:', error);
+          session.user.id = null;
+        }
+      }
+      return session;
+    },
   },
 });
